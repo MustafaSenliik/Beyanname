9136ffc1f02acf8cc005424deb679c051c6d5367
Mustafa Şenlik <105501454+MustafaSenliik@users.noreply.github.com>
2024-10-15 13:52:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/toren'  
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://kullaniciadi:parola@localhost:3306/veritabaniadi'  
 SQLALCHEMY_TRACK_MODIFICATIONS = False  
 
 SECRET_KEY = os.urandom(24)
